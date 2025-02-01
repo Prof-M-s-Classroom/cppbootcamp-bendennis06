1da8d9806fd8b0ee213f94b07f4c45577a359b97
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2025-01-31 20:46:29 -0800

    Add files via upload
    with solutions

--- main.cpp
+++ main.cpp
@@ -11,13 +11,16 @@ private:
 
 public:
     // Constructor
-
+    Robot(string n, string m, int b):name(n), model(m), batteryLife(b){}
 
     // Getter Methods
-
-
+    string getName(){return name;}
+    string getModel(){return model;}
+    int getBatteryLife(){return batteryLife;}
     // Setter Methods
-
+    void setName(string n){name=n;}
+    void setModel(string m){model=m;}
+    void setBatteryLife(int b){batteryLife=b;}
 
     // Display function
     void displayRobot() {
@@ -26,10 +29,14 @@ public:
 };
 
 // Step 2: Function to modify robot (pass by value)
-
+void modifyRobotByValue(Robot r) {
+    r.setBatteryLife(300);
+}
 
 // Step 3: Function to modify robot (pass by reference)
-
+void modifyRobotByReference(Robot &r) {
+    r.setBatteryLife(300);
+}
 
 // Step 4: Template class for a Fleet that stores multiple robots
 template <typename T>
@@ -73,27 +80,27 @@ public:
 
 int main() {
     // Step 5: Create a Robot object
-
-
+Robot terminator = Robot("Robot1", "2025", 100);
+//terminator.displayRobot();
 
     // Step 6: Use pointers to access Robot object
-
-   // cout << "Updated Battery Life (using pointer): " << robotPtr->getBatteryLife() << "%\n";
+        Robot* robotPtr = &terminator;
+        cout<<"Updated Battery Life: "<<robotPtr->getBatteryLife()<<"%\n";
 
     // Step 7: Pass by value (no change outside function)
-
-   // cout << "After modifyRobotByValue, Battery Life: " << myRobot.getBatteryLife() << "%\n";
+        modifyRobotByValue(terminator);
+    cout << "After modifyRobotByValue, Battery Life: " << terminator.getBatteryLife() << "%\n";
 
     // Step 8: Pass by reference (changes persist)
-
-   // cout << "After modifyRobotByReference, Battery Life: " << myRobot.getBatteryLife() << "%\n";
+            modifyRobotByReference(terminator);
+    cout << "After modifyRobotByReference, Battery Life: " << terminator.getBatteryLife() << "%\n";
 
     // Step 9: Use the Fleet template class
     Fleet<string> myFleet(3);
     myFleet.addRobot("Autobot-X");
     myFleet.addRobot("Cybertron-7");
     myFleet.addRobot("NanoDroid-3");
-
+    myFleet.addRobot("GigaChad");
     myFleet.showFleet();
 
     return 0;
